f4991440a4a80bba08a07e35a0de9984b5395cc5
BuildTools <unconfigured@null.spigotmc.org>
2022-08-30 09:56:22 +0530

    Second commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from website import create_app
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 app = create_app()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 if __name__ == '__main__':
     app.run(debug=True)
